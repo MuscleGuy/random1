d61fb897d0fe97d7fdd7871fd50df59956a0b445
MuscleGuy <mrussell@getluma.com>
2017-11-20 09:35:56 -0500

    uploading files

---
+++ bsing/scripts.js
@@ -0,0 +1,241 @@
+$(function() {
+  (function(name) {
+    var container = $('#pagination-' + name);
+    var options = {
+      dataSource: { "portfolio": [
+{
+"project_pic":"./assets/img/portfolio/portfolio1.jpg",
+"project_title":"Project One",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+
+{
+"project_pic":"./assets/img/portfolio/portfolio2.jpg",
+"project_title":"Project Two",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+
+{
+"project_pic":"./assets/img/portfolio/portfolio3.jpg",
+"project_title":"Project Three",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project Four",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+
+{
+"project_pic":"./assets/img/portfolio/portfolio1.jpg",
+"project_title":"Project Five",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+
+{
+"project_pic":"./assets/img/portfolio/portfolio2.jpg",
+"project_title":"Project Six",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+
+{
+"project_pic":"./assets/img/portfolio/portfolio3.jpg",
+"project_title":"Project Seven",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project Eight",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+
+{
+"project_pic":"./assets/img/portfolio/portfolio1.jpg",
+"project_title":"Project Nine",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+
+{
+"project_pic":"./assets/img/portfolio/portfolio2.jpg",
+"project_title":"Project Ten",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+
+{
+"project_pic":"./assets/img/portfolio/portfolio3.jpg",
+"project_title":"Project Eleven",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project Twelve",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project 13",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project 14",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project 15",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project 16",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project 17",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project 18",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project 19",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project 20",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project 21",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project 22",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project 23",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project 24",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project 25",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project 26",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project 27",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+},
+{
+"project_pic":"./assets/img/portfolio/portfolio4.jpg",
+"project_title":"Project 28",
+"project_summary":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+"client_since":"January 2, 2005",
+"project_cat":"Web Design, Graphic Design, SEO"
+}
+]},
+      locator:'portfolio',
+      totalNumber:28,
+      pageSize: 9,
+      callback: function (response, pagination) {
+        var dataHtml ='';
+        $.each(response, function (index, item) {
+
+dataHtml +='<div class="col col-lg-4">';
+dataHtml +='<div class="portfolio-block">';
+dataHtml +='<span class="portfolio-img"><a href="#" alt="" class=""><img src="'+ item.project_pic +'"/></a></span>';
+dataHtml +='<div class="portfolio-details-wrapper">';
+dataHtml +='<span class="portfolio-title">'+ item.project_title;
+dataHtml +='<svg xmlns="http://www.w3.o" height="12" width="12"><polygon points="0,0 12,6 0,12" fill="#FFF" stroke="#FFF" stroke-width="1"></polygon></svg>';
+dataHtml +='</span>';
+dataHtml +='<span class="portfolio-summary"><strong>Summary</strong>' + item.project_summary +'</span>';
+dataHtml +='<span class="portfolio-date"><strong>Client Since</strong>'+ item.client_since +'</span>';
+dataHtml +='<span class="portfolio-category"><i class="fa fa-laptop"></i>'+ item.project_cat +'</span>';
+dataHtml +='</div><!--end portfolio-details-wrapper-->';
+dataHtml +='</div><!--end portfolio block-->';
+dataHtml +='</div><!--end col col-lg-3-->';
+
+        });
+        container.prev().html(dataHtml);
+      }
+};
+container.pagination(options);
+  })('demo1');
+})
---
+++ pagination/my-app/src/app/portfolio.ts
@@ -0,0 +1,97 @@
+{var portfolio = [
+{
+'project_pic':'./assets/img/portfolio/portfolio1.jpg',
+'project_title':'Project One',
+'project_summary':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.',
+'client_since':'January 2, 2005',
+'project_cat':'Web Design, Graphic Design, SEO'
+},
+
+{
+'project_pic':'./assets/img/portfolio/portfolio2.jpg',
+'project_title':'Project Two',
+'project_summary':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.',
+'client_since':'January 2, 2005',
+'project_cat':'Web Design, Graphic Design, SEO'
+},
+
+{
+'project_pic':'./assets/img/portfolio/portfolio3.jpg',
+'project_title':'Project Three',
+'project_summary':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.',
+'client_since':'January 2, 2005',
+'project_cat':'Web Design, Graphic Design, SEO'
+},
+
+{
+'project_pic':'./assets/img/portfolio/portfolio4.jpg',
+'project_title':'Project Four',
+'project_summary':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.',
+'client_since':'January 2, 2005',
+'project_cat':'Web Design, Graphic Design, SEO'
+},
+
+{
+'project_pic':'./assets/img/portfolio/portfolio1.jpg',
+'project_title':'Project Five',
+'project_summary':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.',
+'client_since':'January 2, 2005',
+'project_cat':'Web Design, Graphic Design, SEO'
+},
+
+{
+'project_pic':'./assets/img/portfolio/portfolio2.jpg',
+'project_title':'Project Six',
+'project_summary':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.',
+'client_since':'January 2, 2005',
+'project_cat':'Web Design, Graphic Design, SEO'
+},
+
+{
+'project_pic':'./assets/img/portfolio/portfolio3.jpg',
+'project_title':'Project Seven',
+'project_summary':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.',
+'client_since':'January 2, 2005',
+'project_cat':'Web Design, Graphic Design, SEO'
+},
+
+{
+'project_pic':'./assets/img/portfolio/portfolio4.jpg',
+'project_title':'Project Eight',
+'project_summary':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.',
+'client_since':'January 2, 2005',
+'project_cat':'Web Design, Graphic Design, SEO'
+},
+
+{
+'project_pic':'./assets/img/portfolio/portfolio1.jpg',
+'project_title':'Project Nine',
+'project_summary':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.',
+'client_since':'January 2, 2005',
+'project_cat':'Web Design, Graphic Design, SEO'
+},
+
+{
+'project_pic':'./assets/img/portfolio/portfolio2.jpg',
+'project_title':'Project Ten',
+'project_summary':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.',
+'client_since':'January 2, 2005',
+'project_cat':'Web Design, Graphic Design, SEO'
+},
+
+{
+'project_pic':'./assets/img/portfolio/portfolio3.jpg',
+'project_title':'Project Eleven',
+'project_summary':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.',
+'client_since':'January 2, 2005',
+'project_cat':'Web Design, Graphic Design, SEO'
+},
+
+{
+'project_pic':'./assets/img/portfolio/portfolio4.jpg',
+'project_title':'Project Twelve',
+'project_summary':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.',
+'client_since':'January 2, 2005',
+'project_cat':'Web Design, Graphic Design, SEO'
+}
+]}
